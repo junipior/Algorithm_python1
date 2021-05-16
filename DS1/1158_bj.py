26c06c055d8f442d049838a9e42d8cf7275d30cf
jun <pjk146195@gmail.com>
2021-05-16 21:49:57 +0900

    5월 16일

--- DS1/1158_bj.py
+++ DS1/1158_bj.py
@@ -5,12 +5,12 @@ from collections import deque
 
 deq = deque()
 ans = []
-N, K = map(int, input().split())
+N, K = map(int, input().split()) # map 함수를 활용해 입력 받은 두변수를 띄어쓰기 기준으로 정수형으로 변환
 for i in range(N):
     deq.append(i+1)
 
 while len(deq) > 1:
-    deq.rotate(-(K-1))
+    deq.rotate(-(K-1)) # ex) deque(['q', 'w', 'e', 'r', 't', 'y']), rotate(-2) -> deque(['e','r','t','y','q','w'])
     ans.append(deq[0])
     deq.popleft()
 
